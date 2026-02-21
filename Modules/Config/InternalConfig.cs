5f50e078ce17f6913f4abf475512909a6e9e6955
AdrenSnyder <andrea.cariddi@outlook.com>
2026-02-21 19:58:28 +0100

    Changed ceiling eye timers

--- Modules/Config/InternalConfig.cs
+++ Modules/Config/InternalConfig.cs
@@ -6,8 +6,8 @@ namespace DHHFLastChanceMode.Modules.Config
     internal static class InternalConfig
     {
         internal static bool LastChanceMonstersForceCameraOnLock = true;
-        internal static float LastChanceMonstersCameraLockMaxSeconds = 5f;
-        internal static float LastChanceMonstersCameraLockCooldownSeconds = 15f;
+        internal static float LastChanceMonstersCameraLockMaxSeconds = 3f;
+        internal static float LastChanceMonstersCameraLockCooldownSeconds = 10f;
         internal static float LastChanceMonstersCameraLockKeepAliveGraceSeconds = 0.6f;
         internal static float LastChanceMonstersVisionLockSourceBucketSize = 1f;
     }
