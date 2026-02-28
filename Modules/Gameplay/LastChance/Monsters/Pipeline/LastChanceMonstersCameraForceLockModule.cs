f8a57be73ca175d0775c88aed97c1ce0378fc9d8
AdrenSnyder <andrea.cariddi@outlook.com>
2026-02-28 09:53:28 +0100

    Simplify medium-risk monster transpiler modules
    Removed custom IL pre-scan/token-resolution logic from LastChanceMonstersPlayerVisionCheckModule and LastChanceMonstersCameraForceLockModule.
    Kept explicit target-method lists and typed transpiler rewrites to preserve behavior while reducing opaque patching complexity.
    Updated control-center and review docs to track medium-risk mitigation progress.

--- Modules/Gameplay/LastChance/Monsters/Pipeline/LastChanceMonstersCameraForceLockModule.cs
+++ Modules/Gameplay/LastChance/Monsters/Pipeline/LastChanceMonstersCameraForceLockModule.cs
@@ -15,8 +15,6 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Pipeline
     internal static class LastChanceMonstersCameraForceLockModule
     {
         private const string PatchId = "DHHFLastChanceMode.Gameplay.LastChance.MonstersCameraForceLock";
-        private static readonly System.Reflection.Emit.OpCode[] s_singleByteOpcodes = BuildSingleByteOpcodeMap();
-        private static readonly System.Reflection.Emit.OpCode[] s_doubleByteOpcodes = BuildDoubleByteOpcodeMap();
         private static readonly ManualLogSource Log = Logger.CreateLogSource("DHHFLastChanceMode.LastChance.CeilingEye");
         private static readonly HashSet<System.Reflection.MethodBase> s_patchedMethods = new();
         private static Harmony? s_harmony;
@@ -46,11 +44,6 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Pipeline
             foreach (var method in methods)
             {
                 if (method == null || s_patchedMethods.Contains(method))
-                {
-                    continue;
-                }
-
-                if (!MethodCallsCameraAim(method))
                 {
                     continue;
                 }
@@ -137,180 +130,6 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Pipeline
             }
 
             return list;
-        }
-
-        private static bool MethodCallsCameraAim(System.Reflection.MethodBase method)
-        {
-            if (s_aimTargetSoftSetVanilla == null || s_aimTargetSetVanilla == null)
-            {
-                return false;
-            }
-
-            var body = method.GetMethodBody();
-            var il = body?.GetILAsByteArray();
-            if (il == null || il.Length == 0)
-            {
-                return false;
-            }
-
-            var position = 0;
-            while (position < il.Length)
-            {
-                var opcode = ReadOpcode(il, ref position);
-                if (opcode.Equals(default(System.Reflection.Emit.OpCode)))
-                {
-                    return false;
-                }
-
-                if (opcode.OperandType == System.Reflection.Emit.OperandType.InlineMethod)
-                {
-                    if (position + 4 > il.Length)
-                    {
-                        return false;
-                    }
-
-                    var token = System.BitConverter.ToInt32(il, position);
-                    position += 4;
-                    if (TryResolveMethod(method, token, out var called) &&
-                        (called == s_aimTargetSoftSetVanilla || called == s_aimTargetSetVanilla))
-                    {
-                        return true;
-                    }
-
-                    continue;
-                }
-
-                if (!AdvanceOperand(il, ref position, opcode.OperandType))
-                {
-                    return false;
-                }
-            }
-
-            return false;
-        }
-
-        private static bool TryResolveMethod(System.Reflection.MethodBase sourceMethod, int metadataToken, out System.Reflection.MethodBase? calledMethod)
-        {
-            calledMethod = null;
-            try
-            {
-                var typeArgs = sourceMethod.DeclaringType?.GetGenericArguments();
-                var methodArgs = sourceMethod is System.Reflection.MethodInfo info ? info.GetGenericArguments() : null;
-                calledMethod = sourceMethod.Module.ResolveMethod(metadataToken, typeArgs, methodArgs);
-                return calledMethod != null;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        private static System.Reflection.Emit.OpCode ReadOpcode(byte[] il, ref int position)
-        {
-            if (position >= il.Length)
-            {
-                return default;
-            }
-
-            var b = il[position++];
-            if (b != 0xFE)
-            {
-                return s_singleByteOpcodes[b];
-            }
-
-            if (position >= il.Length)
-            {
-                return default;
-            }
-
-            var b2 = il[position++];
-            return s_doubleByteOpcodes[b2];
-        }
-
-        private static bool AdvanceOperand(byte[] il, ref int position, System.Reflection.Emit.OperandType operandType)
-        {
-            switch (operandType)
-            {
-                case System.Reflection.Emit.OperandType.InlineNone:
-                    return true;
-                case System.Reflection.Emit.OperandType.ShortInlineBrTarget:
-                case System.Reflection.Emit.OperandType.ShortInlineI:
-                case System.Reflection.Emit.OperandType.ShortInlineVar:
-                    position += 1;
-                    return position <= il.Length;
-                case System.Reflection.Emit.OperandType.InlineVar:
-                    position += 2;
-                    return position <= il.Length;
-                case System.Reflection.Emit.OperandType.InlineBrTarget:
-                case System.Reflection.Emit.OperandType.InlineField:
-                case System.Reflection.Emit.OperandType.InlineI:
-                case System.Reflection.Emit.OperandType.InlineSig:
-                case System.Reflection.Emit.OperandType.InlineString:
-                case System.Reflection.Emit.OperandType.InlineTok:
-                case System.Reflection.Emit.OperandType.InlineType:
-                    position += 4;
-                    return position <= il.Length;
-                case System.Reflection.Emit.OperandType.InlineI8:
-                case System.Reflection.Emit.OperandType.InlineR:
-                    position += 8;
-                    return position <= il.Length;
-                case System.Reflection.Emit.OperandType.ShortInlineR:
-                    position += 4;
-                    return position <= il.Length;
-                case System.Reflection.Emit.OperandType.InlineSwitch:
-                    if (position + 4 > il.Length)
-                    {
-                        return false;
-                    }
-
-                    var count = System.BitConverter.ToInt32(il, position);
-                    position += 4 + (count * 4);
-                    return position <= il.Length;
-                default:
-                    return false;
-            }
-        }
-
-        private static System.Reflection.Emit.OpCode[] BuildSingleByteOpcodeMap()
-        {
-            var map = new System.Reflection.Emit.OpCode[256];
-            var fields = typeof(System.Reflection.Emit.OpCodes).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            for (var i = 0; i < fields.Length; i++)
-            {
-                if (fields[i].GetValue(null) is not System.Reflection.Emit.OpCode opcode)
-                {
-                    continue;
-                }
-
-                var value = (ushort)opcode.Value;
-                if (value <= 0xFF)
-                {
-                    map[value] = opcode;
-                }
-            }
-
-            return map;
-        }
-
-        private static System.Reflection.Emit.OpCode[] BuildDoubleByteOpcodeMap()
-        {
-            var map = new System.Reflection.Emit.OpCode[256];
-            var fields = typeof(System.Reflection.Emit.OpCodes).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            for (var i = 0; i < fields.Length; i++)
-            {
-                if (fields[i].GetValue(null) is not System.Reflection.Emit.OpCode opcode)
-                {
-                    continue;
-                }
-
-                var value = (ushort)opcode.Value;
-                if ((value & 0xFF00) == 0xFE00)
-                {
-                    map[value & 0xFF] = opcode;
-                }
-            }
-
-            return map;
         }
 
         internal static void AimTargetSoftSetLastChanceAware(CameraAim? cameraAim, Vector3 position, float inSpeed, float outSpeed, float strengthNoAim, GameObject source, int prio)
