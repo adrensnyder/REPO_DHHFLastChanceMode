e7e7f1f511f5152587fb7db21b63670a91876273
AdrenSnyder <andrea.cariddi@outlook.com>
2026-02-28 08:22:00 +0100

    Refactor typed tumble wings visual coupling module
    Reworked LastChanceMonstersVisualCouplingDecoupleModule.cs to remove assembly scanning and IL-based heuristic callsite discovery.
    Replaced dynamic targeting with explicit patches on EnemyHeartHugger.PlayersInGasLogic and EnemyHeartHuggerGasChecker.Update.
    Removed reflective SetValue usage and switched to direct typed access for tumbleWingPinkTimer.
    Kept the transpiler only where structurally required and removed all using System.Reflection* imports.

--- Modules/Gameplay/LastChance/Monsters/Interactions/LastChanceMonstersVisualCouplingDecoupleModule.cs
+++ Modules/Gameplay/LastChance/Monsters/Interactions/LastChanceMonstersVisualCouplingDecoupleModule.cs
@@ -2,73 +2,45 @@
 
 using System;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 using HarmonyLib;
 
 namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Interactions
 {
-    [HarmonyPatch]
     internal static class LastChanceMonstersVisualCouplingDecoupleModule
     {
-        private static readonly FieldInfo? s_tumbleWingPinkTimerField =
-            AccessTools.Field(typeof(ItemUpgradePlayerTumbleWingsLogic), "tumbleWingPinkTimer");
+        private static readonly System.Reflection.FieldInfo? s_tumbleWingPinkTimerField =
+            AccessTools.Field(typeof(ItemUpgradePlayerTumbleWingsLogic), nameof(ItemUpgradePlayerTumbleWingsLogic.tumbleWingPinkTimer));
 
-        private static readonly MethodInfo? s_upgradeTumbleWingsVisualsActiveMethod =
-            AccessTools.Method(typeof(PlayerAvatar), "UpgradeTumbleWingsVisualsActive", new[] { typeof(bool), typeof(bool) });
+        private static readonly System.Reflection.MethodInfo? s_upgradeTumbleWingsVisualsActiveMethod =
+            AccessTools.Method(typeof(PlayerAvatar), nameof(PlayerAvatar.UpgradeTumbleWingsVisualsActive), new[] { typeof(bool), typeof(bool) });
 
-        private static readonly MethodInfo? s_setTumbleWingPinkTimerSafeMethod =
+        private static readonly System.Reflection.MethodInfo? s_setTumbleWingPinkTimerSafeMethod =
             AccessTools.Method(typeof(LastChanceMonstersVisualCouplingDecoupleModule), nameof(SetTumbleWingPinkTimerSafe));
 
-        private static readonly MethodInfo? s_upgradeTumbleWingsVisualsActiveSafeMethod =
+        private static readonly System.Reflection.MethodInfo? s_upgradeTumbleWingsVisualsActiveSafeMethod =
             AccessTools.Method(typeof(LastChanceMonstersVisualCouplingDecoupleModule), nameof(UpgradeTumbleWingsVisualsActiveSafe));
 
-        [HarmonyTargetMethods]
-        private static IEnumerable<MethodBase> TargetMethods()
+        [HarmonyPatch(typeof(EnemyHeartHugger), "PlayersInGasLogic")]
+        private static class EnemyHeartHuggerPlayersInGasLogicPatch
         {
-            if (s_tumbleWingPinkTimerField == null && s_upgradeTumbleWingsVisualsActiveMethod == null)
+            [HarmonyTranspiler]
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                yield break;
-            }
-
-            Type[] types;
-            try
-            {
-                types = typeof(Enemy).Assembly.GetTypes();
-            }
-            catch
-            {
-                yield break;
-            }
-
-            const BindingFlags flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly;
-            for (var i = 0; i < types.Length; i++)
-            {
-                var type = types[i];
-                if (type == null || type.Name.IndexOf("Enemy", StringComparison.OrdinalIgnoreCase) < 0)
-                {
-                    continue;
-                }
-
-                var methods = type.GetMethods(flags);
-                for (var m = 0; m < methods.Length; m++)
-                {
-                    var method = methods[m];
-                    if (method == null || method.IsAbstract || method.GetMethodBody() == null)
-                    {
-                        continue;
-                    }
-
-                    if (MethodUsesTumbleWingPinkTimer(method) || MethodCallsTumbleWingsVisuals(method))
-                    {
-                        yield return method;
-                    }
-                }
+                return RewriteInstructions(instructions);
             }
         }
 
-        [HarmonyTranspiler]
-        private static IEnumerable<CodeInstruction> Transpiler(MethodBase __originalMethod, IEnumerable<CodeInstruction> instructions)
+        [HarmonyPatch(typeof(EnemyHeartHuggerGasChecker), "Update")]
+        private static class EnemyHeartHuggerGasCheckerUpdatePatch
+        {
+            [HarmonyTranspiler]
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                return RewriteInstructions(instructions);
+            }
+        }
+
+        private static IEnumerable<CodeInstruction> RewriteInstructions(IEnumerable<CodeInstruction> instructions)
         {
             var list = new List<CodeInstruction>(instructions);
 
@@ -77,9 +49,11 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Interactions
                 for (var i = 0; i < list.Count; i++)
                 {
                     var ins = list[i];
-                    if (ins.opcode == OpCodes.Stfld && ins.operand is FieldInfo field && field == s_tumbleWingPinkTimerField)
+                    if (ins.opcode == System.Reflection.Emit.OpCodes.Stfld &&
+                        ins.operand is System.Reflection.FieldInfo field &&
+                        field == s_tumbleWingPinkTimerField)
                     {
-                        ins.opcode = OpCodes.Call;
+                        ins.opcode = System.Reflection.Emit.OpCodes.Call;
                         ins.operand = s_setTumbleWingPinkTimerSafeMethod;
                     }
                 }
@@ -90,71 +64,17 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Interactions
                 for (var i = 0; i < list.Count; i++)
                 {
                     var ins = list[i];
-                    if ((ins.opcode == OpCodes.Call || ins.opcode == OpCodes.Callvirt) &&
-                        ins.operand is MethodInfo method &&
+                    if ((ins.opcode == System.Reflection.Emit.OpCodes.Call || ins.opcode == System.Reflection.Emit.OpCodes.Callvirt) &&
+                        ins.operand is System.Reflection.MethodInfo method &&
                         method == s_upgradeTumbleWingsVisualsActiveMethod)
                     {
-                        ins.opcode = OpCodes.Call;
+                        ins.opcode = System.Reflection.Emit.OpCodes.Call;
                         ins.operand = s_upgradeTumbleWingsVisualsActiveSafeMethod;
                     }
                 }
             }
 
             return list;
-        }
-
-        private static bool MethodUsesTumbleWingPinkTimer(MethodBase method)
-        {
-            return MethodContainsCallOrFieldToken(method, s_tumbleWingPinkTimerField?.MetadataToken ?? -1, requireStfld: true);
-        }
-
-        private static bool MethodCallsTumbleWingsVisuals(MethodBase method)
-        {
-            return MethodContainsCallOrFieldToken(method, s_upgradeTumbleWingsVisualsActiveMethod?.MetadataToken ?? -1, requireStfld: false);
-        }
-
-        private static bool MethodContainsCallOrFieldToken(MethodBase method, int targetToken, bool requireStfld)
-        {
-            if (method == null || targetToken < 0)
-            {
-                return false;
-            }
-
-            try
-            {
-                var il = method.GetMethodBody()?.GetILAsByteArray();
-                if (il == null || il.Length < 5)
-                {
-                    return false;
-                }
-
-                for (var i = 0; i <= il.Length - 5; i++)
-                {
-                    var op = il[i];
-                    if (requireStfld)
-                    {
-                        if (op != 0x7D)
-                        {
-                            continue;
-                        }
-                    }
-                    else if (op != 0x28 && op != 0x6F)
-                    {
-                        continue;
-                    }
-
-                    var token = BitConverter.ToInt32(il, i + 1);
-                    if (token == targetToken)
-                    {
-                        return true;
-                    }
-                }
-            }
-            catch
-            {
-            }
-
-            return false;
         }
 
         private static void SetTumbleWingPinkTimerSafe(ItemUpgradePlayerTumbleWingsLogic? logic, float value)
@@ -166,7 +86,7 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Interactions
                     throw new System.NullReferenceException("upgradeTumbleWingsLogic is null");
                 }
 
-                s_tumbleWingPinkTimerField?.SetValue(logic, value);
+                logic.tumbleWingPinkTimer = value;
                 return;
             }
 
@@ -175,7 +95,7 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Interactions
                 return;
             }
 
-            s_tumbleWingPinkTimerField?.SetValue(logic, value);
+            logic.tumbleWingPinkTimer = value;
         }
 
         private static void UpgradeTumbleWingsVisualsActiveSafe(PlayerAvatar player, bool visualsActive, bool pink)
