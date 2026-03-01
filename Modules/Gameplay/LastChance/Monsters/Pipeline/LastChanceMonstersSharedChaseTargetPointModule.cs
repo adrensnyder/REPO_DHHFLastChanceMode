4147bbe0b11224d699cb0e18a914bb3fcf84202f
AdrenSnyder <andrea.cariddi@outlook.com>
2026-03-01 02:35:47 +0100

    Replace shared chase target-point transpiler with typed chase state prefixes
    Removed IL transpiler logic from LastChanceMonstersSharedChaseTargetPointModule and introduced typed Harmony prefixes for EnemyStateChase.Update and EnemyStateChaseBegin.Update. Reimplemented chase flow with LastChance-effective target positions through the targeting orchestrator, preserving runtime behavior without reflection-based opcode rewriting.

--- Modules/Gameplay/LastChance/Monsters/Pipeline/LastChanceMonstersSharedChaseTargetPointModule.cs
+++ Modules/Gameplay/LastChance/Monsters/Pipeline/LastChanceMonstersSharedChaseTargetPointModule.cs
@@ -1,6 +1,5 @@
 #nullable enable
 
-using System.Collections.Generic;
 using DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Support;
 using HarmonyLib;
 using UnityEngine;
@@ -10,65 +9,258 @@ namespace DHHFLastChanceMode.Modules.Gameplay.LastChance.Monsters.Pipeline
     [HarmonyPatch]
     internal static class LastChanceMonstersSharedChaseTargetPointModule
     {
-        private static readonly List<System.Reflection.MethodBase> s_targetMethods =
-            LastChanceMonstersPatchTargetHelper.BuildTargetList(AddTargetMethods);
-
-        private static readonly System.Reflection.MethodInfo? s_transformGetPositionMethod =
-            AccessTools.PropertyGetter(typeof(Transform), nameof(Transform.position));
-
-        private static readonly System.Reflection.MethodInfo? s_effectiveTransformPositionMethod =
-            AccessTools.Method(typeof(LastChanceMonstersSharedChaseTargetPointModule), nameof(GetEffectiveTransformPosition));
-
-        [HarmonyTargetMethods]
-        private static IEnumerable<System.Reflection.MethodBase> TargetMethods()
+        [HarmonyPatch(typeof(EnemyStateChase), nameof(EnemyStateChase.Update))]
+        internal static class EnemyStateChaseUpdatePatch
         {
-            return s_targetMethods;
-        }
-
-        private static void AddTargetMethods(List<System.Reflection.MethodBase> methods)
-        {
-            LastChanceMonstersPatchTargetHelper.AddDeclaredMethod(methods, typeof(EnemyStateChase), nameof(EnemyStateChase.Update));
-            LastChanceMonstersPatchTargetHelper.AddDeclaredMethod(methods, typeof(EnemyStateChaseBegin), nameof(EnemyStateChaseBegin.Update));
-            LastChanceMonstersPatchTargetHelper.AddDeclaredMethod(methods, typeof(EnemyStateChaseSlow), nameof(EnemyStateChaseSlow.Update));
-        }
-
-        [HarmonyPrepare]
-        private static bool Prepare()
-        {
-            return s_transformGetPositionMethod != null && s_effectiveTransformPositionMethod != null;
-        }
-
-        [HarmonyTranspiler]
-        private static IEnumerable<CodeInstruction> ReplaceTargetPositionReads(IEnumerable<CodeInstruction> instructions)
-        {
-            if (s_transformGetPositionMethod == null || s_effectiveTransformPositionMethod == null)
+            [HarmonyPrefix]
+            private static bool Prefix(EnemyStateChase __instance)
             {
-                return instructions;
-            }
-
-            var list = new List<CodeInstruction>(instructions);
-            for (var i = 0; i < list.Count; i++)
-            {
-                var ins = list[i];
-                if ((ins.opcode != System.Reflection.Emit.OpCodes.Call && ins.opcode != System.Reflection.Emit.OpCodes.Callvirt) || ins.operand is not System.Reflection.MethodInfo called)
+                if (!LastChanceMonstersTargetProxyHelper.IsRuntimeEnabled())
                 {
-                    continue;
+                    return true;
                 }
 
-                if (called == s_transformGetPositionMethod)
+                ExecuteChaseUpdate(__instance);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(EnemyStateChaseBegin), nameof(EnemyStateChaseBegin.Update))]
+        internal static class EnemyStateChaseBeginUpdatePatch
+        {
+            [HarmonyPrefix]
+            private static bool Prefix(EnemyStateChaseBegin __instance)
+            {
+                if (!LastChanceMonstersTargetProxyHelper.IsRuntimeEnabled())
                 {
-                    ins.opcode = System.Reflection.Emit.OpCodes.Call;
-                    ins.operand = s_effectiveTransformPositionMethod;
+                    return true;
                 }
+
+                ExecuteChaseBeginUpdate(__instance);
+                return false;
+            }
+        }
+
+        private static void ExecuteChaseUpdate(EnemyStateChase state)
+        {
+            if (!state.Enemy.MasterClient)
+            {
+                return;
             }
 
-            return list;
+            if (state.Enemy.CurrentState != EnemyState.Chase)
+            {
+                if (state.Active)
+                {
+                    state.Active = false;
+                }
+
+                return;
+            }
+
+            var targetPlayer = state.Enemy.TargetPlayerAvatar;
+            if (targetPlayer == null)
+            {
+                state.Enemy.CurrentState = EnemyState.Roaming;
+                return;
+            }
+
+            if (!state.Active)
+            {
+                targetPlayer.LastNavMeshPositionTimer = 0f;
+                state.ChasePosition = ResolveTargetPosition(targetPlayer);
+                state.VisionTimer = state.VisionTime;
+                state.ChaseCanReachSet = false;
+                state.SawPlayerHide = false;
+                state.CantReachTime = 0f;
+                state.StateTimer = Random.Range(state.StateTimeMin, state.StateTimeMax);
+                state.Active = true;
+            }
+
+            state.Enemy.SetChaseTimer();
+            state.Enemy.NavMeshAgent.UpdateAgent(state.Speed, state.Acceleration);
+            if (state.Enemy.Vision.VisionTriggered[targetPlayer.photonView.ViewID])
+            {
+                state.VisionTimer = state.VisionTime;
+            }
+            else if (state.VisionTimer > 0f)
+            {
+                state.VisionTimer -= Time.deltaTime;
+            }
+
+            var effectiveTargetPosition = ResolveTargetPosition(targetPlayer);
+            if (state.VisionTimer > 0f)
+            {
+                if (state.ChaseOnlyOnNavmesh || targetPlayer.LastNavMeshPositionTimer <= 0.25f)
+                {
+                    state.Enemy.NavMeshAgent.Enable();
+                    state.Enemy.NavMeshAgent.SetDestination(targetPlayer.LastNavmeshPosition);
+                    if (state.ChaseCanReachSet)
+                    {
+                        var point = state.Enemy.NavMeshAgent.GetPoint();
+                        state.ChaseCanReach = Vector3.Distance(point, effectiveTargetPosition) <= 0.5f;
+
+                        if (targetPlayer.isCrawling && !state.ChaseCanReach && SemiFunc.EnemyLookUnderCondition(state.Enemy, state.StateTimer, 5f, targetPlayer))
+                        {
+                            state.SawPlayerHidePosition = effectiveTargetPosition;
+                            state.SawPlayerNavmeshPosition = targetPlayer.LastNavmeshPosition;
+                            state.SawPlayerHide = true;
+                        }
+
+                        state.ChasePosition = point;
+                    }
+
+                    state.ChaseCanReachSet = true;
+                }
+                else
+                {
+                    state.Enemy.NavMeshAgent.Disable(0.1f);
+                    state.transform.position = Vector3.MoveTowards(state.transform.position, effectiveTargetPosition, state.Speed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                if (state.SawPlayerHide)
+                {
+                    state.Enemy.CurrentState = EnemyState.LookUnder;
+                    return;
+                }
+
+                state.Enemy.NavMeshAgent.SetDestination(state.ChasePosition);
+                if (Vector3.Distance(state.transform.position, state.ChasePosition) < 1f)
+                {
+                    var levelPointAhead = state.Enemy.GetLevelPointAhead(state.ChasePosition);
+                    if (levelPointAhead)
+                    {
+                        state.Enemy.NavMeshAgent.SetDestination(levelPointAhead.transform.position);
+                    }
+
+                    state.ChasePosition = state.Enemy.NavMeshAgent.GetDestination();
+                }
+
+                state.ChaseCanReach = true;
+                state.ChaseCanReachSet = false;
+            }
+
+            if (state.ChaseCanReach && state.Enemy.Vision.VisionsTriggered[targetPlayer.photonView.ViewID] >= state.VisionsToReset)
+            {
+                state.StateTimer = Random.Range(state.StateTimeMin, state.StateTimeMax);
+            }
+
+            if (!state.ChaseCanReach)
+            {
+                state.CantReachTime += Time.deltaTime;
+                if (state.CantReachTime > 2f)
+                {
+                    state.Enemy.Vision.VisionsTriggered[targetPlayer.photonView.ViewID] = 0;
+                    state.Enemy.CurrentState = EnemyState.ChaseSlow;
+                    return;
+                }
+            }
+            else
+            {
+                state.CantReachTime = 0f;
+            }
+
+            state.StateTimer -= Time.deltaTime;
+            if (state.StateTimer <= 0f)
+            {
+                state.Enemy.CurrentState = EnemyState.ChaseSlow;
+            }
+
+            if (targetPlayer.isDisabled)
+            {
+                state.Enemy.Vision.VisionsTriggered[targetPlayer.photonView.ViewID] = 0;
+                state.Enemy.CurrentState = EnemyState.Roaming;
+            }
         }
 
-        private static Vector3 GetEffectiveTransformPosition(Transform transform)
+        private static void ExecuteChaseBeginUpdate(EnemyStateChaseBegin state)
         {
-            return LastChanceMonstersTargetingOrchestrator.ResolveEffectiveTransformTargetPoint(transform);
+            if (state.Enemy.CurrentState != EnemyState.ChaseBegin)
+            {
+                if (state.Active)
+                {
+                    state.Active = false;
+                }
+
+                return;
+            }
+
+            if (!state.Active)
+            {
+                if (state.Enemy.MasterClient)
+                {
+                    state.Enemy.StateChase.ChaseCanReach = true;
+                    state.Enemy.NavMeshAgent.ResetPath();
+                    state.StateTimer = Random.Range(state.StateTimeMin, state.StateTimeMax);
+                }
+
+                state.TargetPlayer = PlayerController.instance.playerAvatarScript;
+                foreach (var playerAvatar in GameDirector.instance.PlayerList)
+                {
+                    if (!playerAvatar.isDisabled && playerAvatar.photonView.ViewID == state.Enemy.TargetPlayerViewID)
+                    {
+                        state.TargetPlayer = playerAvatar;
+                        break;
+                    }
+                }
+
+                foreach (var playerAvatar in GameDirector.instance.PlayerList)
+                {
+                    if (!playerAvatar.isDisabled && playerAvatar.isLocal)
+                    {
+                        if (GameManager.instance.gameMode != 0 && !(state.TargetPlayer == playerAvatar) && !state.Enemy.PlayerRoom.SameLocal && !state.Enemy.OnScreen.OnScreenLocal)
+                        {
+                            state.LocalEffect = false;
+                            GameDirector.instance.CameraImpact.ShakeDistance(5f, 5f, 10f, state.transform.position, 0.25f);
+                            GameDirector.instance.CameraShake.ShakeDistance(3f, 5f, 10f, state.transform.position, 0.5f);
+                            break;
+                        }
+
+                        state.LocalEffect = true;
+                        GameDirector.instance.CameraImpact.Shake(5f, 0.25f);
+                        GameDirector.instance.CameraShake.Shake(3f, 0.5f);
+                        if (state.Stinger)
+                        {
+                            CameraGlitch.Instance.PlayShort();
+                            AudioScare.instance.PlayImpact();
+                        }
+
+                        break;
+                    }
+                }
+
+                state.Active = true;
+            }
+
+            state.Enemy.SetChaseTimer();
+            if (!state.Enemy.MasterClient)
+            {
+                return;
+            }
+
+            var targetPlayer = state.TargetPlayer;
+            if (targetPlayer == null)
+            {
+                state.Enemy.CurrentState = EnemyState.Roaming;
+                return;
+            }
+
+            state.Enemy.NavMeshAgent.UpdateAgent(0f, 5f);
+            state.Enemy.NavMeshAgent.Stop(0.1f);
+            state.transform.LookAt(ResolveTargetPosition(targetPlayer));
+            state.transform.localEulerAngles = new Vector3(0f, state.transform.localEulerAngles.y, 0f);
+            state.StateTimer -= Time.deltaTime;
+            if (state.StateTimer <= 0f)
+            {
+                state.Enemy.CurrentState = EnemyState.Chase;
+            }
         }
 
+        private static Vector3 ResolveTargetPosition(PlayerAvatar targetPlayer)
+        {
+            return LastChanceMonstersTargetingOrchestrator.ResolveEffectiveTransformTargetPoint(targetPlayer.transform);
+        }
     }
 }
